edbb4945d8326b830e66fe8a74418d44201e8a60
terehsieh <42392544+terehsieh@users.noreply.github.com>
2020-11-11 08:39:36 -0300

    Q&A

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//ACORDEON
+
+document.addEventListener("DOMContentLoaded", () => {
+    let lis = document.querySelectorAll("ul.acordeon li");
+    lis.forEach(li => {
+        li.addEventListener("click", () => {
+            li.nextElementSibling.classList.toggle("visible");
+        });
+    });
+});
