a2daf4baaa7121ad8f1f5bda218674d22b937bdc
terehsieh <42392544+terehsieh@users.noreply.github.com>
2020-11-15 00:08:31 -0300

    planes, registrar, index

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 //ACORDEON
 
 document.addEventListener("DOMContentLoaded", () => {
+    //acordion
     let lis = document.querySelectorAll("ul.acordeon li");
     lis.forEach(li => {
         li.addEventListener("click", () => {
             li.nextElementSibling.classList.toggle("visible");
         });
     });
+
+    //form 
+
+    //password
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#password');
+
+    togglePassword.addEventListener('click', function(e) {
+        // toggle the type attribute
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        // toggle the eye slash icon
+        this.classList.toggle('fa-eye-slash');
+    });
+
+
 });
